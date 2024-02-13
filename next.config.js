c52413938d765b9d50a31853dfe3dcb622737eec
yokesh_ks <yokesh.ks@increscotech.com>
2024-02-13 09:48:54 +0530

    feat: docs setup and content added

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+}
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+})
+
+module.exports = withNextra(nextConfig)
---
+++ theme.config.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { DocsThemeConfig } from "nextra-theme-docs"
+
+const config: DocsThemeConfig = {
+  logo: <span>TailwindXpress</span>,
+  footer: {
+    text: "IngeniousClan",
+  },
+}
+
+export default config
